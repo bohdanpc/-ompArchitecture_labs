9d77c0619949a5d4122a17c9e94469e8973c84e1
bohdanpc <bohdanpc@gmail.com>
2017-04-08 19:16:50 +0300

    general improvement of model:
    add find_by_date() method;
    add new record value - 'date';
    general refactor

--- Lab1/model.py
+++ Lab1/model.py
@@ -1,29 +1,35 @@
-# TODO NESHTA: add general structure and maybe pickle-file reading/writing
-
 import _pickle
 
 
-class Record( object ) :
-    def __init__( self , _data , _length , _coefficient ) :
+class Record(object):
+    def __init__(self, date, _data, _length, _coefficient):
+        self.date = date
         self.data = _data
         self.length = _length
         self.coefficient = _coefficient
 
-    def get( self ) :
-        return [ self.data , self.length , self.coefficient ]
+    def get(self):
+        return [self.data, self.length, self.coefficient]
 
 
-def initialise( file_name ) :
-    try :
-        with open( file_name , 'rb' ) as f :
-            list = _pickle.load( f )
-            f.close( )
-        return list
-    except Exception as error :
-        return [ ]
+def initialise(file_name):
+    try:
+        with open(file_name, 'rb') as f:
+            records = _pickle.load(f)
+            f.close()
+        return records
+    except Exception as error:
+        return []
 
 
-def save_all( list , file_name ) :
-    with open( file_name , 'wb' ) as f :
-        _pickle.dump( list , f , _pickle.HIGHEST_PROTOCOL )
-    f.close( )
+def save_all(records, file_name):
+    with open(file_name, 'wb') as f:
+        _pickle.dump(records, f, _pickle.HIGHEST_PROTOCOL)
+    f.close()
+
+
+def find_by_date(records, date):
+    for item in records:
+        if item.date == date:
+            return item
+    return False
