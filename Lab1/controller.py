fda103a8b98676f5bbbb5b6cb35dc2ef1d2f794a
bohdanpc <bohdanpc@gmail.com>
2017-04-10 18:23:29 +0300

    ---finish main project--- implement all sub-actions for main menu add all checks for entered values refactor some logic of model:     replace some FALSE values to [] - empty list

--- Lab1/controller.py
+++ Lab1/controller.py
@@ -14,25 +14,43 @@ def show_summary(records):
 
 def show_summary_period(records):
     left, right = view.enter_period()
-    # records_period = model.get_records_period(left, right, records)
-    # view.print_summary(get_general_length(records_period), get_general_fuel_used(records_period)
+    if check_validity_of_date(left) and check_validity_of_date(right):
+        records_period = find_by_date_range(records, left, right)
+        show_summary(records_period)
+    else:
+        view.invalid_value()
 
 
 def show_by_date(records):
-    pass
+    date = view.enter_date()
+    if check_validity_of_date(date):
+        records_daily = find_by_date(records, date)
+        show_all(records_daily)
+    else:
+        view.invalid_value()
 
 
 def show_by_period(records):
-    pass
+    left, right = view.enter_period()
+    if check_validity_of_date(left) and check_validity_of_date(right):
+        records_period = find_by_date_range(records, left, right)
+        show_all(records_period)
+    else:
+        view.invalid_value()
 
 
-def add_record(item_list, records):
-    record = Record(item_list[0], item_list[1], item_list[2])
-    records.append(record)
+def add_record(records):
+    item_list = view.enter_trip_details()
+    if check_validity_of_date(item_list[0]) and \
+            check_validity_of_length(item_list[1]) and check_validity_of_coefficient(item_list[2]):
+        record = Record(item_list[0], item_list[1], item_list[2])
+        records.append(record)
+    else:
+        view.invalid_value()
 
 
 def main_func():
-    """ readiness level: 1, 4, 7"""
+    """ readiness level: 1, 4, 5, 7"""
     choice = ''
     records = initialise("fuel_consumption.pickle")
     while choice != "7":
@@ -46,9 +64,9 @@ def main_func():
         elif choice == "4":
             show_summary_period(records)
         elif choice == "5":
-            pass
+            show_by_date(records)
         elif choice == "6":
-            add_record(view.enter_trip_details(), records)
+            add_record(records)
         elif choice == "7":
             save_all(records, "fuel_consumption.pickle")
         else:
