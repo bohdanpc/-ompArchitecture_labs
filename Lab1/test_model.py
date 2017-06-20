a3071ba52da9c8216bd0888b7ab8a00aecdc197b
Ruslan Neshta <ruslan.neshta@gmail.com>
2017-06-21 00:01:41 +0300

    Fixed unittests issures in model unittests

--- Lab1/test_model.py
+++ Lab1/test_model.py
@@ -18,7 +18,7 @@ class TestModel(unittest.TestCase):
     def test_check_validity(self):
         self.assertTrue(self.model.check_validity(
             [self.model.records[0].date, self.model.records[0].length, self.model.records[0].coefficient]))
-        self.assertFalse(self.model.check_validity(["10-09-2015", "12", 10]))
+        self.assertFalse(self.model.check_validity(["10-29-2015", "12", 10]))
 
     def test_compare_date(self):
         self.assertEqual(self.model.compare_date(self.model.records[0].date, "10-09-2015"), 2)
@@ -26,9 +26,11 @@ class TestModel(unittest.TestCase):
         self.assertEqual(self.model.compare_date(self.model.records[0].date, "10-09-2040"), 0)
 
     def test_find_by_date(self):
-        elemennts = [model.Model.Record("12-01-2017", 125, 3.14), model.Model.Record("12-03-2017", 250, 14.20), \
-                     model.Model.Record("12-03-2017", 456, 55.11)]
-        self.assertEqual(self.model.find_by_date(elemennts, "12-03-2017")[0].length, 250)
+        self.model.records.append(model.Model.Record("12-01-2017", 125, 3.14))
+        self.model.records.append(model.Model.Record("12-03-2017", 250, 14.20))
+        self.model.records.append(model.Model.Record("12-03-2017", 456, 55.11))
+        result = self.model.find_by_date("12-03-2017")
+        self.assertEqual(result[0].length, 250)
 
     def test_find_by_date_range(self):
         elements = [model.Model.Record("12-01-2017", 125, 3.14), model.Model.Record("12-02-2017", 250, 14.20), \
