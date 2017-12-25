a8fcf6c36ba1b25b92f1ed4abce8f6067ecf304c
s1lence <Ruslan.Neshta@gmail.com>
2017-12-25 23:20:53 +0200

    erase update

--- lab1_sem2/tree/tree.go
+++ lab1_sem2/tree/tree.go
@@ -1,7 +1,11 @@
 package tree
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 type Abstract interface {
@@ -11,93 +15,124 @@ type Abstract interface {
 	Equals(abstract Abstract) bool
 }
 
-type node struct {
-	left, right, parent *node
-	data                Abstract
+type Node struct {
+	left, right, parent *Node
+	Data                Abstract
 }
 
 type Tree struct {
-	root *node
+	root *Node
 }
 
 func (t *Tree) Add(field Abstract) {
 
 	if t.root == nil {
-		t.root = new(node)
-		t.root.data = field
+		t.root = new(Node)
+		t.root.Data = field
 		t.root.parent = t.root
 		return
 	}
 
 	for p := t.root; p != nil; {
 		p_parent := p
-		if p.data.Less(field) {
+		if p.Data.Less(field) {
 			p = p.left
 		} else {
 			p = p.right
 		}
 		if p == nil {
-			if p_parent.data.Less(field) {
-				p_parent.left = new(node)
-				p_parent.left.data = field
+			if p_parent.Data.Less(field) {
+				p_parent.left = new(Node)
+				p_parent.left.Data = field
 				p_parent.left.parent = p_parent
 			} else {
-				p_parent.right = new(node)
-				p_parent.right.data = field
+				p_parent.right = new(Node)
+				p_parent.right.Data = field
 				p_parent.right.parent = p_parent
 			}
 		}
 	}
 }
 
-func (t *Tree) Erase(p *node) {
+func (ptr *Node) excludeChildrenTo(what *Node) {
+	if ptr == ptr.parent.right {
+		ptr.parent.right = what
+	} else {
+		ptr.parent.left = what
+	}
+	if what != nil {
+		what.parent = ptr.parent
+	}
+}
+func (ptr *Node) excludeParent() {
+	if ptr == ptr.parent.right {
+		ptr.parent.right = nil
+	} else {
+		ptr.parent.left = nil
+	}
+	ptr.parent = nil
+}
+
+func (t *Tree) Erase(p *Node) {
 	if p.left != nil {
-		tmp := t.max(p.left)
-		p.data = tmp.data
-		if tmp == tmp.parent.right {
-			tmp.parent.right = tmp.left
-		} else {
-			tmp.parent.left = tmp.left
-		}
-		if tmp.left != nil {
-			tmp.left.parent = tmp.parent
-		}
+		ptr := t.max(p.left)
+		p.Data = ptr.Data
+		ptr.excludeChildrenTo(ptr.left)
 	} else {
 		if p.right == nil {
-			if p.parent.left == p {
+			p.excludeParent()
+		} else {
+			ptr := t.min(p.right)
+			p.Data = ptr.Data
+			ptr.excludeChildrenTo(ptr.right)
+		}
+	}
+}
+
+/*
+if p.parent.left == p {
 				p.parent.left = nil
 			} else {
 				p.parent.right = nil
 			}
 			p.parent = nil
-		} else {
-			tmp := t.min(p.right)
-			p.data = tmp.data
-			if tmp == tmp.parent.left {
-				tmp.parent.left = tmp.right
-			} else {
-				tmp.parent.right = tmp.right
-			}
-			if tmp.right != nil {
-				tmp.right.parent = tmp.parent
-			}
-		}
-	}
+
+if ptr == ptr.parent.right {
+ptr.parent.right = ptr.left
+} else {
+ptr.parent.left = ptr.left
+}
+if ptr.left != nil {
+ptr.left.parent = ptr.parent
 }
 
-func (t *Tree) Find(abstract Abstract) *node {
+if ptr == ptr.parent.left {
+				ptr.parent.left = ptr.right
+			} else {
+				ptr.parent.right = ptr.right
+			}
+			if ptr.right != nil {
+				ptr.right.parent = ptr.parent
+			}
+
+*/
+
+func (t *Tree) Find(abstract Abstract) *Node {
 	tmp := t.root
-	for !tmp.data.Equals(abstract) {
-		if tmp.data.Less(abstract) {
+	for !tmp.Data.Equals(abstract) {
+		if tmp.Data.Less(abstract) {
 			tmp = tmp.left
 		} else {
 			tmp = tmp.right
+		}
+		if tmp == nil {
+			return tmp
 		}
 	}
 	return tmp
 }
 
-func (t *Tree) min(p *node) *node {
+func (t *Tree) min(p *Node) *Node {
 	if p.left != nil {
 		tmp := p.left
 		for ; tmp.left != nil; tmp = tmp.left {
@@ -107,7 +142,7 @@ func (t *Tree) min(p *node) *node {
 	return p
 }
 
-func (t *Tree) max(p *node) *node {
+func (t *Tree) max(p *Node) *Node {
 	if p.right != nil {
 		tmp := p.right
 		for ; tmp.right != nil; tmp = tmp.right {
@@ -117,12 +152,12 @@ func (t *Tree) max(p *node) *node {
 	return p
 }
 
-func (t *Tree) printTree(p *node, depth int) {
+func (t *Tree) printTree(p *Node, depth int) {
 	if p != nil {
 		for i := 0; i < depth; i++ {
 			fmt.Print("	")
 		}
-		fmt.Println(p.data)
+		fmt.Println(p.Data)
 		t.printTree(p.left, depth+1)
 		t.printTree(p.right, depth+1)
 	}
@@ -130,4 +165,26 @@ func (t *Tree) printTree(p *node, depth int) {
 
 func (t *Tree) Print() {
 	t.printTree(t.root, 0)
+}
+
+func (t *Tree) AddFromFile() {
+	file, err := os.Open("/home/thereptile/" +
+		"GoglandProjects/CompArchitecture_labs/lab1_sem2/info.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		ucl := strings.ToUpper(scanner.Text())
+		fmt.Println(ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
 }
